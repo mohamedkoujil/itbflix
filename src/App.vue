70b62988f8bd6c18704197440fb03d46320c8aec
jacomar <jacomar304@hotmail.com>
2024-05-06 10:03:14 +0200

    'actor.vue' component done

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <h1>Películas destacadas</h1>
   <section class="flex wrap center">
     <carta_pelicula 
       imagen = "sawx.jpg"
@@ -70,10 +71,70 @@
       @clickPelicula="clicDiv"
     />
   </section>
+
+  <h1>Actores destacados</h1>
+    <section class="flex wrap center">
+      <carta_actor 
+        imagen="cate_blanchett.webp"
+        nombre="Cate Blanchett" 
+        puntuacion="8.5/10"
+        :peliculas="['El señor de los anillos', 'Carol', 'Blue Jasmine']"
+        @clickActor="clicDiv"
+      />
+
+      <carta_actor 
+        imagen="ewan_mcgregor.webp"
+        nombre="Ewan McGregor"
+        puntuacion="8.3/10"
+        :peliculas="['Trainspotting', 'Moulin Rouge!', 'Star Wars']"
+        @clickActor="clicDiv"
+      />
+
+      <carta_actor 
+        imagen="johnny_depp.webp"
+        nombre="Johnny Depp"
+        puntuacion="7.9/10"
+        :peliculas="['Piratas del Caribe', 'Eduardo Manostijeras', 'Charlie y la fábrica de chocolate']"
+        @clickActor="clicDiv"
+      />
+
+      <carta_actor 
+        imagen="helena_bonham_carter.webp"
+        nombre="Helena Bonham Carter"
+        puntuacion="8.0/10"
+        :peliculas="['El discurso del rey', 'Harry Potter', 'Fight Club']"
+        @clickActor="clicDiv"
+      />
+
+      <carta_actor
+      imagen="leonardo_dicaprio.webp"
+      nombre="Leonardo DiCaprio"
+      puntuacion="9.0"
+      :peliculas="['Titanic', 'El lobo de Wall Street', 'Origen']"
+      @clickActor="clickActor"
+      />
+
+    <carta_actor
+      imagen="scarlett_johansson.jpg"
+      nombre="Scarlett Johansson"
+      puntuacion="8.7"
+      :peliculas="['Avengers: Endgame', 'Lost in Translation', 'Jojo Rabbit']"
+      @clickActor="clickActor"
+      />
+
+    <carta_actor
+      imagen="christina_ricci.jpg"
+      nombre="Christina Ricci"
+      puntuacion="8.5"
+      :peliculas="['Sleepy Hollow', 'Monster', 'La familia Addams']"
+      @clickActor="clickActor"
+      />
+    </section>
 </template>
 
 <script>
 import carta_pelicula from './components/pelicula.vue'
+import carta_actor from './components/actor.vue'
 
 export default {
   name: 'App',
@@ -83,7 +144,8 @@ export default {
     }
   },
   components: {
-    carta_pelicula
+    carta_pelicula,
+    carta_actor
   },
   methods: {
     clicDiv() {
---
+++ src/components/actor.vue
@@ -0,0 +1,95 @@
+<template>
+    <div class="actor-container justify-between" @click="$emit('clickActor')">
+      <img :src="require(`../assets/${imagen}`)" alt="Imagen del actor/actriz" />
+      <h2>{{ nombre }}</h2>
+      <div class="peliculas">
+        <h3>Películas destacadas:</h3>
+        <ul>
+          <li v-for="pelicula in peliculas" :key="pelicula">{{ pelicula }}</li>
+        </ul>
+      </div>
+      <div class="puntuacion">{{ puntuacion }}</div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "carta_actor",
+    props: {
+      imagen: String,
+      nombre: String,
+      puntuacion: String,
+      peliculas: Array,
+    },
+  };
+  </script>
+  
+  <style>
+  .actor-container {
+    display: flex;
+    flex-direction: column;
+    width: 160px;
+    height: 550px;
+    padding: 10px;
+    margin: 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    box-shadow: 0px 0px 12px rgb(255, 0, 191);
+    cursor: pointer;
+    transition: 0.3s;
+    background-color: #102c54;
+  }
+  
+  .actor-container img {
+    width: auto;
+    height: 200px;
+    box-shadow: 0px 0px 12px rgb(255, 0, 191);
+    border-radius: 50%;
+    margin: auto;
+    width: 150px;
+  }
+  
+  .actor-container:hover {
+    box-shadow:  -7px 8px 9px rgb(255, 0, 191);
+    transform: translate(6px, -8px);
+  }
+  
+  h2 {
+    font-size: 1.2em;
+    color: #ccc;
+    margin: 10px 0;
+  }
+  
+  .puntuacion {
+    font-size: 1.2em;
+    margin: 0 -10px -10px -10px;
+    background-color: aqua;
+    padding: 6px;
+    border-radius: 4px;
+  }
+  
+  .peliculas {
+    margin-top: auto;
+  }
+  
+  .peliculas h3 {
+    font-size: 1em;
+    color: #ccc;
+  }
+  
+  .peliculas ul {
+    list-style-type: none;
+    padding-left: 0;
+  }
+  
+  .peliculas li {
+    font-size:smaller;
+    background-color: #004275;
+    color: #CCC;
+    margin-left: 20px;
+  }
+  
+  .justify-between {
+    justify-content: space-between;
+  }
+  </style>
